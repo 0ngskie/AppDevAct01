b6c0203a9e3209884a87856c4f8c88316ba9eff8
0ngskie <ongjohnbenedict2@gmail.com>
2025-01-12 00:16:32 +0800

    First commit

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return (
+        <footer>
+            <p>&copy 2025 AppDevAct || OK Enterprise</p>
+        </footer>
+    );
+}
---
+++ pages/about.js
@@ -0,0 +1,21 @@
+import Header from 'components\Header';
+import Header from 'components\Navbar';
+import Header from 'components\Footer';
+
+export default function About(){
+    return(
+        <div>
+
+            <h1> 
+                About Page
+            </h1>
+
+            <p> 
+                Hello and welcome to the About Page of my application. <br />
+                I haven't add much information in this page about the app,<br />
+                but maybe in the future I can design a better one.
+            </p>
+
+        </div>
+    );
+}
